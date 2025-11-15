595ba03bc161a2e8a964de609633ef0dfc79e106
Railanecs <railanesilva322@gmail.com>
2025-11-14 22:18:31 -0300

    algoritmos de estrutura de dados

--- atividade1.js
+++ atividade1.js
@@ -1,5 +1,9 @@
-/* Solicite o peso da pessoa e informe o consumo de agua recomendada
- Escreva o resultado em um popup*/
- let peso = parseFloat(prompt("Informe seu peso"))
- let agua = 0.03*peso
- alert (`Consumo de água ideal: ${agua}`)
+let A = parseInt(prompt("Informe o valor de A"))
+let B = parseInt(prompt("Informe o valor de B"))
+let C = parseInt(prompt("Informe o valor de C"))
+if (A+b<c){
+    alert("A soma de A + B é menor que C")
+}
+else {
+    alert ("A soma de A + B é não menor que C")
+}
--- atividade2.js
+++ atividade2.js
@@ -1,21 +1,12 @@
-/* Pergunte as horas (somente as horas). Se a hora estiver ate as 11
- escreva a hora e insira AM.
- 2 Se a hora informada for maior que 12, converta para sistema de horas
- ate 12 e escreva PM. Se a hora for igual a 12 escreva meio-dia e se
- for igual a 00 escreva meia-noite */
-  let horas = parseInt(prompt("Informe as horas"))
-  let pm = horas-12
-  if (horas >=1 && horas <=11){
-    alert (`Horário: ${horas} AM`)
-  }
-  else if (horas > 12 && horas <24){
-    alert (`Horário : ${pm} PM `)
-  }
-  else if (horas == 12){
-    alert (`Meio-dia`)
-  }
-  else if (horas == 0){
-    alert (`Meia-noite`)
-  }
-  else 
-  { alert (`Hora Inválida`)}
+let nome = prompt("Digite seu nome")
+let sexo = prompt ("Digite o sexo (F ou M)")
+let estadoCivil = prompt ("Informe seu estado civil")
+
+if (sexo=="F" && estadoCivil =="casada"){
+    let tempoCasada = parseInt(prompt ("Informe seu tempo de casada em anos"))
+     
+    alert (`Nome: ${nome} \n Sexo: ${sexo}\n Estado civil: ${estadoCivil}\n Anos casada: ${tempoCasada}`)
+}
+else {
+    alert (`Nome: ${nome} \n Sexo: ${sexo}\n Estado civil: ${estadoCivil}`)
+}
